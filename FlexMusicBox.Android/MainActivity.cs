c57da74bd64272272528a0d1b0505a91e1c5ad90
Fantom TM <QWERTYkez@outlook.com>
2021-04-12 20:50:43 +0700

    Fonts Correction

--- FlexMusicBox.Android/MainActivity.cs
+++ FlexMusicBox.Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using Android.Util;
 using MediaManager;
 
 namespace FlexMusicBox.Droid
@@ -17,8 +18,22 @@ namespace FlexMusicBox.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            initFontScale();
             LoadApplication(new App());
         }
+
+        private void initFontScale()
+        {
+            var configuration = Resources.Configuration;
+            configuration.FontScale = (float)1;
+            //0.85 small, 1 standard, 1.15 big，1.3 more bigger ，1.45 supper big 
+            var metrics = new DisplayMetrics();
+            WindowManager.DefaultDisplay.GetMetrics(metrics);
+            metrics.ScaledDensity = configuration.FontScale * metrics.Density;
+            BaseContext.Resources.UpdateConfiguration(configuration, metrics);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
